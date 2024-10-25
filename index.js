07ee03ab223d43ed70ab6583f94827b7803043b1
Trần Văn Linh <133183567+LINHTRAN9x@users.noreply.github.com>
2024-10-25 22:38:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ client.once('ready', () => {
     console.log('Bot is online!');
     
     postMemeToChannel();
-    setInterval(postMemeToChannel, 2 * 60 * 60 * 1000); 
+    setInterval(postMemeToChannel, 30 * 60 * 1000); 
 });
 
 client.login(token);
